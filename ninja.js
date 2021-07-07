920c4bafe928a0cd57c003073f9764487f8d2f5d
krisbratvold <kris.bratvold@gmail.com>
2021-07-07 12:15:50 -0700

    added sensei

--- ninja.js
+++ ninja.js
@@ -18,8 +18,24 @@ class Ninja{
         this.health += 10;
     }
 }
+class Sensei extends Ninja{
+    constructor(name){
+        super(name)
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
 
 const ninja1 = new Ninja("Hyabusa");
+const superSensei = new Sensei("Master Splinter");
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStats();
+superSensei.speakWisdom();
+superSensei.showStats();
