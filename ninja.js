66faeb98701220f4f6899927c286de4df4adb706
krisbratvold <kris.bratvold@gmail.com>
2021-07-07 12:06:47 -0700

    initial commit

---
+++ ninja.js
@@ -0,0 +1,25 @@
+class Ninja{
+    constructor(name){
+        this.name = name;
+        this.health = 50;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName(){
+        console.log(this.name)
+    }
+    showStats(){
+        console.log("Name:", this.name)
+        console.log("Health:", this.health)
+        console.log("Speed:", this.speed)
+        console.log("Strength:", this.strength)
+    }
+    drinkSake(){
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.drinkSake();
+ninja1.showStats();
